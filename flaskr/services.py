c23d4b1243a766750b25e6e38100acd7cf1533ac
Lucas <lucas.tamoios@gmail.com>
2019-07-22 15:38:44 -0300

    Refactors function get_spaceship_data

--- flaskr/services.py
+++ flaskr/services.py
@@ -3,16 +3,27 @@ import requests
 
 def get_spaceship_data():
     page = 0
-    url = 'https://swapi.co/api/starships/'
     content = []
     while True:
         page += 1
-        r = requests.get(url, params={'page': page})
-        page_content = r.json()
-        content.extend([
-            {'name': x['name'], 'hyperdrive': x['hyperdrive_rating']}
-            for x in page_content['results']
-        ])
-        if not page_content['next']:
+        page_content, is_there_next_page = _get_content_from_page(page)
+        content.extend(page_content)
+        if not is_there_next_page:
             break
     return content
+
+
+def _get_content_from_page(page_number):
+    url = 'https://swapi.co/api/starships/'
+    r = requests.get(url, params={'page': page_number})
+    page_content = r.json()
+    return _parse_page_content(page_content), bool(page_content['next'])
+
+
+def _parse_page_content(page_content):
+    """Returns the needed content from the given page"""
+    return [
+        # List of dicts with name and hyperdrive
+        {'name': x['name'], 'hyperdrive': x['hyperdrive_rating']}
+        for x in page_content['results']
+    ]
